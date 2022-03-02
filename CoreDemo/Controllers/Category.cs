8c65fd60a91eee5d44c06ecb3fa32d9320ae1e0c
CihatDaghan <88133939+CihatDaghan@users.noreply.github.com>
2022-03-02 14:35:11 +0300

    proje oluşturuldu .

--- CoreDemo/Controllers/Category.cs
+++ CoreDemo/Controllers/Category.cs
@@ -4,10 +4,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace CoreDemo.Controllers
 {
     public class Category : Controller
     {
+        
         public IActionResult Index()
         {
             return View();
