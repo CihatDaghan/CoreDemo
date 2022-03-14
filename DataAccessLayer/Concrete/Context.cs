b4c05912c11974c46bc10fef929a02ce42ce7535
CihatDaghan <88133939+CihatDaghan@users.noreply.github.com>
2022-03-14 16:59:53 +0300

    yazar ile mesajlar arasında bağlantı kuruldu ve controller buna göre düzenlendi.

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -14,6 +14,22 @@ namespace DataAccessLayer.Concrete
         {
             optionsBuilder.UseSqlServer("server=DESKTOP-2RFMJGK\\SQLEXPRESS;database=CoreBlogDb;integrated security=true;");
         }
+
+        //Writer tablosu ile Message2 tablosu arasında 2li ilişki kurulması için gerekli
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Message2>()
+                .HasOne(x => x.SenderUser)
+                .WithMany(y => y.WriterSender)
+                .HasForeignKey(z => z.SenderID)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+            modelBuilder.Entity<Message2>()
+                .HasOne(x => x.ReceiverUser)
+                .WithMany(y => y.WriterReceiver)
+                .HasForeignKey(z => z.ReceiverID)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+        }
+        //
         public DbSet<About> Abouts { get; set; }
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Category> Categories { get; set; }
@@ -24,5 +40,6 @@ namespace DataAccessLayer.Concrete
         public DbSet<BlogRayting> BlogRaytings { get; set; }
         public DbSet<Notification> Notifications { get; set; }
         public DbSet<Message> Messages { get; set; }
+        public DbSet<Message2> Message2s { get; set; }
     }
 }
