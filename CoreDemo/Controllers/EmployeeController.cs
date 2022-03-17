de5bf1b1c92e420a8f5f37cd4dfa66f4393a8464
CihatDaghan <88133939+CihatDaghan@users.noreply.github.com>
2022-03-17 17:13:08 +0300

    projeye api katmanı ekledi ve projeler arası ilişki kuruldu

---
+++ CoreDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync("https://localhost:44311/api/Default");
+            var jsonString = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<Class>>(jsonString);
+            return View(values);
+        }
+    } 
+    public class Class
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
